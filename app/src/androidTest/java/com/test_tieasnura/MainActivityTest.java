b8698423c7437e3c0d8f6d22a108da08029b6bd8
tieas angraeni <tieasnura@gmail.com>
2022-09-27 10:49:36 +0700

    adding test unit

---
+++ app/src/androidTest/java/com/test_tieasnura/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.test_tieasnura;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void products() {
+    }
+}
